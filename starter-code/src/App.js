a47de9872bd1a698d927c7a86bdcb347981fab53
levijskal <levijskal@icloud.com>
2018-09-26 09:42:37 -0400

    x

--- starter-code/src/App.js
+++ starter-code/src/App.js
@@ -1,18 +1,53 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bulma/css/bulma.css';
+import foods from './foods.json'
+import FoodBox from './FoodBox.js'
 
 class App extends Component {
+constructor(props) {
+  super(props)
+  this.state={
+    foodsList: foods
+  }
+}
+
+
+
+showFoods = () => {
+return this.state.foodsList.map((eachFood, index) => {
+  return <FoodBox key={index} foodName ={eachFood.name} calories={eachFood.calories} image={eachFood.image}/>
+})  
+}
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+<form>
+<div class="field">
+  <label class="label">Name</label>
+  <div class="control">
+    <input class="input" type="text"/>
+  </div>
+</div>
+
+<div class="field">
+  <label class="label">Calories</label>
+  <div class="control">
+    <input class="input" type="number"/>
+  </div>
+
+  <button>submit</button>
+</div>
+</form>
+
+{this.showFoods()}
+
+      
+      
+      
       </div>
     );
   }
